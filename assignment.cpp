abbfcbb737f059ff8d7ca8c82d1a94f5aa2b1e60
YasmeenWafa <yasmeen_wafa@hotmail.com>
2016-10-18 23:35:35 +0300

    haa7

--- assignment.cpp
+++ assignment.cpp
@@ -1,19 +1,24 @@
 #include <stdlib.h>
-
 #include <glut.h>
-#include <stdio.h> 
+#include <stdio.h>
+
 #include <math.h>
 #include <sstream>
 #include <windows.h>
+#include <string> 
 
-
+int count = 0;
+char *specialStatus = "Not ready";
+double height;
+double width;
+int time;
 double x;
 double y;
 double scx = 500;
 double scy = 500;
-boolean doublescore= false;
-boolean chasersstun = false;
-boolean specialpower = false;
+boolean doublescore;
+boolean chasersstun;
+boolean specialpower;
 double xo=0;
 double yo=0;
 double xcord = 225;
@@ -35,11 +40,14 @@ double VectorX;
 double VectorY;
 const double PI = 3.141592653589793238463;
 double angle;
+double tt;
 
 void Display(void);
 void Anim(void);
 void move(int,int);
-void exit(unsigned char key, int x, int y);
+void keyboard(unsigned char key, int x, int y);
+void displayText(float x, float y, int r, int g, int b, const char *string);
+
 
 void main(int argc, char** argr)
 {
@@ -47,9 +55,13 @@ void main(int argc, char** argr)
 
 	glutInit(&argc, argr);
 	  glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-	  glutInitWindowSize(scx, scy);
-	 
-	  
+	 // glutInitWindowSize(scx, scy);
+	 // height = glutGet(GLUT_SCREEN_HEIGHT);
+	  //width = glutGet(GLUT_SCREEN_WIDTH);
+
+	  height = 500;
+	  width = 500;
+	  glutInitWindowSize(width, height);
 
 	  glutInitWindowPosition(50, 50);
 	  glutCreateWindow("assignment");
@@ -58,10 +70,13 @@ void main(int argc, char** argr)
 	  glutIdleFunc(Anim);
 	  glPointSize(25);
 	  glutPassiveMotionFunc(move);
-	  glutKeyboardFunc(exit);
+	  glutKeyboardFunc(keyboard);
+	 // glutFullScreen();
+	 
 	 
 	  glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
-	  gluOrtho2D(0.0, 500.0, 0.0, 500.0);
+	  gluOrtho2D(0.0, height, 0.0, width);
+	  //gluOrtho2D(0.0, 500.0, 0.0, 500.0);
 	  glutMainLoop();
 	
 }
@@ -71,9 +86,21 @@ void Display(void)
 {
 	glClear(GL_COLOR_BUFFER_BIT);
 
-	glPushMatrix();
+	/*glPushMatrix();
 	glColor3f(1.0f, 1.0f, 0.0f);
+	std::string tt;
+	tt = std::to_string(glutGet(GLUT_ELAPSED_TIME));
+	char *s = new char[50];
+	strcpy(s, tt.c_str);
 	
+
+	displayText(100, 100, 1, 0, 1, s);
+	glPopMatrix();*/
+
+	glPushMatrix();
+	displayText(100, width-100, 1, 0, 1, specialStatus);
+	glPopMatrix();
+
 
 	glPushMatrix();
 	glColor3f(1.0f, 1.0f, 0.0f);
@@ -190,13 +217,15 @@ void Display(void)
 	glEnd();
 	glPopMatrix();
 
-
+	
 
 	glFlush();
 }
 
 void Anim(void)
 {
+	
+
 	if ((x >= xcord-50 && x<= xcord)) {
 		x = 0;
 		y = 0;
@@ -227,40 +256,42 @@ void Anim(void)
 		else score += 2;
 
 	}
-	if (x_ <= scx && x_ >= scx-1)
+	if ((x_ <= width && x_ >= width-1 )|| x_ <= 0)
 	{
 		x_ = xcord +50;
 		y_ = ycord +50;
 	}
-	if (x1_ <= scx && x1_ >= scx-1)
+	if ((x1_ <= width && x1_ >= width-1) || x1_<=0)
 	{
 		x1_ = xcord + 50;
 		y1_ = ycord + 50;
 	}
 	
 	
-	vecX = xcord - x;
-	vecY = ycord - y;
+	vecX = 2*(xcord - x);
+	vecY = 2*(ycord - y);
 	
-	vecX_ = x_ - xcord;
-	vecY_ = y_ - ycord;
+	vecX_ = 2*(x_ - xcord);
+	vecY_ = 2*(y_ - ycord);
 
 	double length = sqrt((vecX*vecX) + (vecY*vecY));
 	double length_ = sqrt((vecX_*vecX_) + (vecY_*vecY_));
 
-	vecXo = xcord - xo;
-	vecYo = ycord - yo;
+	vecXo = 2*(xcord - xo);
+	vecYo = 2*(ycord - yo);
 
-	vecX1_ = x1_ - xcord;
-	vecY1_ = y1_ - ycord;
+	vecX1_ = 2*(x1_ - xcord);
+	vecY1_ = 2*(y1_ - ycord);
 
 	double lengtho = sqrt((vecXo*vecXo) + (vecYo*vecYo));
 	double length1_ = sqrt((vecX1_*vecX1_) + (vecY1_*vecY1_));
 
-	x += (vecX/length)-0.65;
-	y += (vecY/length)-0.65;
-	xo += (vecXo / lengtho) - 0.5;
-	yo += (vecYo / lengtho) - 0.5;
+	if (chasersstun == false) {
+		x += (vecX / length) - 0.65;
+		y += (vecY / length) - 0.65;
+		xo += (vecXo / lengtho) - 0.5;
+		yo += (vecYo / lengtho) - 0.5;
+	}
 
 	x_ += (vecX_ / length_)-0.65;
 	y_ -= (vecY_ / length_)-0.75;
@@ -273,39 +304,54 @@ void Anim(void)
 	if (glutGet(GLUT_ELAPSED_TIME) == 300000) //5 minutes
 	{
 		printf("hi");
-		glutEnterGameMode(); // it stops execution
-	
-		
-
+		exit(0); // it stops execution
 	}
-	if (glutGet(GLUT_ELAPSED_TIME) == 30000)
+
+	if ((count >= 1))
 	{
-		if (specialpower == false) {
-			while (glutGet(GLUT_ELAPSED_TIME) <= 60000)
-			{
-				doublescore = true;
-			}
-		}
-		else {
-			while (glutGet(GLUT_ELAPSED_TIME) <= 60000)
-			{
-				xo = 0;
-				yo = 0;
-				x  =  0;
-				y = 0;
-			}
-		}
+		specialStatus = "No more powers!";
+	}
+	if ((count == 0) && (glutGet(GLUT_ELAPSED_TIME) >= 30000) && (glutGet(GLUT_ELAPSED_TIME) < 60000))
+	{
+		specialStatus = "ready";
+	}
+	if ((count == 0) && (glutGet(GLUT_ELAPSED_TIME) < 30000))
+	{
+		specialStatus = "Not ready";
 	}
 
+	if (tt >= 30000)
+	{
+		if (specialpower == true) {
+
+			if (doublescore == true || chasersstun ==true) {
+				
+				specialStatus = "active";
+				if (tt + 30000 < glutGet(GLUT_ELAPSED_TIME))
+				{
+					doublescore = false;
+					chasersstun = false;
+					specialpower = false;
+					
+					specialStatus = "No more powers!";
+				}
+				
+			}
+		
+		}
+		
+	}
+	
 	
 
 	glutPostRedisplay();
 }
 
+
 void move(int xx, int yy)
 {
-	xcord = xx - 25;
-	ycord = 500 - yy - 25;
+	xcord = xx -25;
+	ycord = (width) - yy - 25;
 
 	VectorX =  xcord-xx;
 	VectorY = ycord - yy;
@@ -315,19 +361,47 @@ void move(int xx, int yy)
 	
 }
 
-void exit(unsigned char key, int x, int y) {
-	if (key == 27) //escape key to terminate
+void keyboard(unsigned char key, int x, int y) {
+
+	switch (key)
 	{
-		exit(0);
+	case (27) : exit(0); break;
+
+	case('f') : glutFullScreen(); break;
+	case ('d') : if (count < 1) {
+		specialpower = true;
+		doublescore = true;
+		 tt = glutGet(GLUT_ELAPSED_TIME);
+
+		count++;
 	}
-	if (key == 'f') { //to enter full screen
-		glutFullScreen();
-		scx = 1366;
-			scy = 768;
-		
+				 break;
+	case('c') : if (count < 1) {
+
+		specialpower = true;
+		chasersstun = true;
+		count++;
+		 tt = glutGet(GLUT_ELAPSED_TIME);
+
+	}
+				break;
 	}
 	
+	
+	
 }
+
+void displayText(float x, float y, int r, int g, int b, const char *string)
+{
+	int j = strlen(string);
+	glColor3f(r, g, b);
+	glRasterPos2f(x, y);
+	for (int i = 0; i < j; i++)
+	{
+		glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, string[i]);
+	}
+}
+
 
 
 
